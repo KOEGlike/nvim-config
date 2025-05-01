fe9dbc565596c32671313640aff474b5e3e13cf0
Balázs Marcell <KoegLike@proton.me>
2025-05-01 23:38:22 +0300

    removed rust tools

--- lua/custom/plugins/rust-analyzer.lua
+++ lua/custom/plugins/rust-analyzer.lua
@@ -1,24 +1,13 @@
 return {
-    -- Core LSP config plugin
-    {
-      "neovim/nvim-lspconfig",
-      ft = { "rust" },
-      config = function()
-        require("lspconfig").rust_analyzer.setup({
-          settings = { ["rust-analyzer"] = {} }
-        })
-      end
-    },
-    -- Rust-specific enhancements (uses rust-analyzer)
-    {
-      "simrat39/rust-tools.nvim",
-      ft = { "rust" },
-      opts = {
-        -- Any rust-tools settings, e.g. inlay hints, hover actions, etc.
-      },
-      config = function(_, opts)
-        require("rust-tools").setup(opts)
-      end
-    },
-  }
-  
+  -- Core LSP config plugin
+  {
+    'neovim/nvim-lspconfig',
+    ft = { 'rust' },
+    config = function()
+      require('lspconfig').rust_analyzer.setup {
+        settings = { ['rust-analyzer'] = {} },
+      }
+    end,
+  },
+}
+
