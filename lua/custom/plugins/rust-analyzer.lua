ee43e1559997e23670ca2abc489bdbd3c55f4ff1
Balázs Marcell <KoegLike@proton.me>
2025-05-02 14:38:46 +0300

    removed rust analyzer

--- lua/custom/plugins/rust-analyzer.lua
+++ lua/custom/plugins/rust-analyzer.lua
@@ -1,15 +1,6 @@
 return {
   -- Core LSP config plugin
   {
-    'neovim/nvim-lspconfig',
-    ft = { 'rust' },
-    config = function()
-      require('lspconfig').rust_analyzer.setup {
-        settings = { ['rust-analyzer'] = {} },
-      }
-    end,
-  },
-  {
     'mrcjkb/rustaceanvim',
     version = '^6',
     lazy = false,
